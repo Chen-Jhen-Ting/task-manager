a0cb0a0d27d82e58b26c78d412f8401cf4d830ad
Ted Chen <teddy70642000@gmail.com>
2020-11-16 21:15:01 +0800

    application.html.erb - add   - notice   - alert
    view
    - users
      - sign_up
        - form for create user
    
    controller
    - users
      - sign_up
      - create
        - succeed -> show notice
        - fail -> render sign_up
      - private method
        - clean_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+  def sign_up
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(clean_params)
+    if @user.save
+      redirect_to root_path, notice: 'succeed to sign up'
+    else
+      render :sign_up
+    end
+  end
+
+  private
+
+  def clean_params
+    params.require(:user).permit(:email, :name, :password)
+  end
 end
