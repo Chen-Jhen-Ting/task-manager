69a97884273015d4162eda75ea954d85d40a2adb
Ted Chen <teddy70642000@gmail.com>
2020-11-16 21:25:48 +0800

    User model - validate   - name, at least 2   - email, fit email reqex   - password,  at least 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :name, length: { minimum: 2 }
+  validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
+  message: "Your email format is wrong" }
+  validates :password, length: { minimum: 6 }
+
+
 end
