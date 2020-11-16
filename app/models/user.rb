8d8821dcfa1a90baa97b58a46120d295813defb0
Ted Chen <teddy70642000@gmail.com>
2020-11-16 21:38:28 +0800

    User model - validates   - email     - uniquness: true
    - private method
      - encrypt_password -> save encrypt password as new password
      - change  ->  use sha1 to encrypt password
    
    - before_create
      - encrypt_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 class User < ApplicationRecord
   validates :name, length: { minimum: 2 }
-  validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
+  validates :email, uniqueness: true, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
   message: "Your email format is wrong" }
   validates :password, length: { minimum: 6 }
 
+  before_create :encrypt_password
 
+  private
+
+  def encrypt_password
+    self.password = change(password)
+  end
+
+  def change(str)
+    Digest::SHA1.hexdigest(str)
+  end
 end
