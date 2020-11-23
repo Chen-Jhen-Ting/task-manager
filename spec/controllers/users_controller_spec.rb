a0224bd7fa2a8902e864e0b5edac1db6c2aa9f41
Ted Chen <teddy70642000@gmail.com>
2020-11-23 18:06:56 +0800

    Rspec User controller sign_in, sign_up

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe '#sign_in' do
+    subject {get :sign_in}
+    it 'the link should be ok' do
+      expect(subject.status).to be 200
+    end
+  end
+
+  describe '#sign_up' do
+    subject {get :sign_up}
+    it 'the link should be ok' do
+      expect(subject.status).to be 200
+    end
+  end
 
 end
