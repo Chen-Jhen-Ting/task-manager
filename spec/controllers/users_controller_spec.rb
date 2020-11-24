e334e48c7b0853e0884e5a8f2a3af3992d95628f
Ted Chen <teddy70642000@gmail.com>
2020-11-24 11:53:03 +0800

    Rspec user controller - #login - #create

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,6 +7,44 @@ RSpec.describe UsersController, type: :controller do
       expect(subject.status).to be 200
     end
   end
+  
+  describe '#login' do
+    before do 
+      @user = User.create(
+        name: '陳振庭',
+        email: 'zxcvzxcvzxcv@gmail.com',
+        password:'zxcvzcxv'
+      )
+    end
+    subject {post :login, params: params}
+    context 'with correct email and password' do
+      let(:params) do
+        {
+          user:{
+            email: 'zxcvzxcvzxcv@gmail.com',
+            password:'zxcvzcxv'
+          }
+        }
+      end
+      it 'should find user' do
+        expect(subject.status).to be 302
+      end
+    end
+    
+    context 'with wrong email and password' do
+      let(:params) do
+        {
+          user:{
+            email: 'zcvzxcv@gmail.com',
+            password:'zxcvzc'
+          }
+        }
+      end
+      it 'should not find user' do
+        expect(subject.status).to be 200
+      end
+    end
+  end
 
   describe '#sign_up' do
     subject {get :sign_up}
@@ -15,4 +53,41 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
+  describe '#create' do
+    subject {post :create, params: params}
+
+    context 'with appropriate data' do
+      let(:params) do
+        {
+          user:{
+            name: '陳振庭',
+            email: 'zcvzxcv@gmail.com',
+            password:'zxcvzc'
+          }
+        }
+      end
+      it 'user should be created' do
+        expect(subject.status).to be 302
+        expect(User.last.name).to eq '陳振庭' 
+      end
+    end
+
+    context ' with inappropriate data' do
+      let(:params) do
+        {
+          user:{
+            name: '陳庭',
+            email: 'zcvzxcv@gmail.com',
+            password:'zxcc'
+          }
+        }
+      end
+      it 'user should not be created' do
+        expect(subject.status).to be 200
+        expect(User.last.name).not_to eq '陳庭'
+      end
+    end
+  end
+
+  
 end
